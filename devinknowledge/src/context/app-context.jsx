0e1ca84c58186c0e5149723f30ca8cc5324104eb
Darlan Martins <langodarlan46@gmail.com>
2022-09-07 16:43:24 -0300

    Exer 8

--- devinknowledge/src/context/app-context.jsx
+++ devinknowledge/src/context/app-context.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 
 const appContext = React.createContext(null);
 const ContextProvider = appContext.Provider;
@@ -9,7 +10,37 @@ export const useAppContext = () => {
 };
 
 export const AppContextProvider = ({ children }) => {
+  const [allTips, setAllTips] = useState([
+    {
+      titulo: "Uma dica",
+      descricao:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi, quo.",
+      categoria: "front-end",
+      linguagem: "css (flexbox)",
+      video: "https://www.youtube.com/watch?v=3elGSZSWTbM",
+    },
+  ]);
+  const [filter, setFilter] = useState(null);
+
+  const createTip = (tip) => {
+    setAllTips((pTips = [...pTips, tip]));
+  };
+
+  const filterTip = (query) => {
+    if (query) {
+      setFilter(query);
+    } else {
+      setFilter(null);
+    }
+  };
+
+  const tips = filter
+    ? allTips.filter((tip) => tip.titulo.includes(filter))
+    : allTips;
+
   return (
-    <ContextProvider value={{ message: "Olá" }}>{children}</ContextProvider>
+    <ContextProvider value={{ filterTip, createTip, tips }}>
+      {children}
+    </ContextProvider>
   );
 };
