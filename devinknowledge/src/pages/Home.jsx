1978553192d0d643d98ece5733df52989dba0cab
Darlan Martins <langodarlan46@gmail.com>
2022-09-07 15:06:06 -0300

    first commit

---
+++ devinknowledge/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import { MainContent } from "../components/MainContent";
+import { Sidebar } from "../components/Sidebar";
+
+export const Home = () => {
+  return (
+    <div className="root-container">
+      <Sidebar />
+      <MainContent />
+    </div>
+  );
+};
